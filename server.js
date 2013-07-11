ec5dd3d8f5318724242e44119d493df27adb6053
John Manero <jmanero@dyn.com>
2013-07-11 11:31:23 -0400

    Add favicon

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var Express = require('express');
+var FS = require('fs');
 var Optimist = require('optimist');
 var Path = require('path');
 
@@ -12,11 +13,17 @@ var options = Optimist
 	['default']('l', false)
 	.argv;
 
-var server = Express();
-server.use(Express.logger());
+var icon = FS.readFileSync('./favicon.ico');
 
+var server = Express();
+
+server.use(Express.logger());
 server.use(Express.directory(Path.resolve(__dirname, options.target)));
 server.use(Express.static(Path.resolve(__dirname, options.target)));
+
+server.get('/favicon.ico', function(req, res, next) {
+	res.send(icon);
+});
 
 server.listen(options.port, (options.local ? "127.0.0.1" : null), function() {
 	console.log("Listening on " + (options.local ? "localhost:" : "*:") + options.port);
